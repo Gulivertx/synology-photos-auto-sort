6919e165a8148eabf93bb02930241c729eba9133
Gulivertx <me@gulivert.ch>
2020-04-29 14:11:22 +0200

    Tasks improvments

--- synology-photos-auto-sort.sh
+++ synology-photos-auto-sort.sh
@@ -6,7 +6,7 @@
 # https://github.com/Gulivertx/synology-photos-auto-sort #
 ##########################################################
 
-VERSION="0.2"
+VERSION="0.3"
 PID_FILE="/tmp/synology_photos_auto_sort.pid"
 ERROR_DIRECTORY="error"
 
@@ -64,66 +64,105 @@ echo "Start image process"
 echo ""
 
 for EXT in "${IMG_EXT[@]}"; do
-  FILES_COUNTER=$(ls *.$EXT 2> /dev/null | wc -l)
+    FILES_COUNTER=$(ls *.$EXT 2> /dev/null | wc -l)
 
-  if [ $FILES_COUNTER != 0 ]; then
-    for FILE in *.$EXT; do
-      DATETIME=$(exiftool "$FILE" | grep -i "create date" | head -1 | xargs)
-      DATE=${DATETIME:14:10}
-      TIME=${DATETIME:25:8}
-      NEW_NAME=${DATE//:}_${TIME//:}.${EXT,,}
+    if [ $FILES_COUNTER != 0 ]; then
+        for FILE in *.$EXT; do
+            DATETIME=$(exiftool "$FILE" | grep -i "create date" | head -1 | xargs)
 
-      YEAR=${DATE:0:4}
-      MONTH=${DATE:5:2}
+            # Verify if we have exif data available
+            if [ -z "$DATETIME" ]; then
+                continue
+            fi
 
-      # Create target folder
-      mkdir -p $TARGET/$YEAR/$YEAR.$MONTH
-      mv -n "$FILE" $TARGET/$YEAR/$YEAR.$MONTH/$NEW_NAME
-    done
-    wait
-    echo "All $EXT have been moved"
-    echo ""
-  fi
+            # Extract date, time year and month and build new filename
+            DATE=${DATETIME:14:10}
+            TIME=${DATETIME:25:8}
+            NEW_NAME=${DATE//:}_${TIME//:}.${EXT,,}
+
+            # Create target folder
+            YEAR=${DATE:0:4}
+            MONTH=${DATE:5:2}
+            mkdir -p $TARGET/$YEAR/$YEAR.$MONTH
+
+            # Move the file to target folder
+            mv -n "$FILE" $TARGET/$YEAR/$YEAR.$MONTH/$NEW_NAME
+        done
+
+        # Wait until the copy process is done
+        wait
+
+        echo "All $EXT have been moved"
+        echo ""
+    fi
 done
 
 echo "Start video process"
 echo ""
 
 for EXT in "${VIDEO_EXT[@]}"; do
-  FILES_COUNTER=$(ls *.$EXT 2> /dev/null | wc -l)
+    FILES_COUNTER=$(ls *.$EXT 2> /dev/null | wc -l)
 
-  if [ $FILES_COUNTER != 0 ]; then
-    for FILE in *.$EXT; do
-      DATETIME=$(exiftool "$FILE" | grep -i "create date" | head -1 | xargs)
-      DATE=${DATETIME:14:10}
-      TIME=${DATETIME:25:8}
-      NEW_NAME=${DATE//:}_${TIME//:}.${EXT,,}
+    if [ $FILES_COUNTER != 0 ]; then
+        for FILE in *.$EXT; do
+            DATETIME=$(exiftool "$FILE" | grep -i "create date" | head -1 | xargs)
 
-      YEAR=${DATE:0:4}
-      MONTH=${DATE:5:2}
+            # Verify if we have exif data available
+            if [ -z "$DATETIME" ]; then
+                continue
+            fi
 
-      # Create target folder
-      mkdir -p $TARGET/$YEAR/$YEAR.$MONTH
-      mv -n "$FILE" $TARGET/$YEAR/$YEAR.$MONTH/$NEW_NAME
-    done
-    wait
-    echo "All $EXT have been moved"
-    echo ""
-  fi
+            # Extract date, time year and month and build new filename
+            DATE=${DATETIME:14:10}
+            TIME=${DATETIME:25:8}
+            NEW_NAME=${DATE//:}_${TIME//:}.${EXT,,}
+
+            # Create target folde
+            YEAR=${DATE:0:4}
+            MONTH=${DATE:5:2}
+            mkdir -p $TARGET/$YEAR/$YEAR.$MONTH
+
+            # Move the file to target folder
+            mv -n "$FILE" $TARGET/$YEAR/$YEAR.$MONTH/$NEW_NAME
+        done
+
+        # Wait until the copy process is done
+        wait
+
+        echo "All $EXT have been moved"
+        echo ""
+    fi
 done
 
-### Move all files still not moved by the above rules
+### Move all files still not moved by the above rules in an error folder
 UNMOVED_FILES_COUNTER=$(ls *.* 2> /dev/null | wc -l | xargs)
 
 if [ $UNMOVED_FILES_COUNTER != 0 ]; then
-  echo "There is $UNMOVED_FILES_COUNTER unmoved files, these files will be moved into error folder"
-  echo ""
+    echo "There is $UNMOVED_FILES_COUNTER unmoved files, these files will be moved into error folder"
+    echo ""
 
-  mkdir -p $SOURCE/$ERROR_DIRECTORY
+    mkdir -p $SOURCE/$ERROR_DIRECTORY
 
-  for FILE in *.*; do
-    mv "$FILE" $SOURCE/$ERROR_DIRECTORY/"$FILE"
-  done
+    for FILE in *.*; do
+        # Get file extension
+        EXT="${FILE##*.}"
+
+        DATETIME=$(exiftool "$FILE" | grep -i "create date" | head -1 | xargs)
+
+        # Verify if we have exif data available
+        if [ -z "$DATETIME" ]; then
+            echo "No exif data available for image $FILE"
+            echo "Use original filename"
+            FILENAME="${FILE%.*}"
+            NEW_FILENAME=${FILENAME=//:}_exif_data_missing.${EXT,,}
+        else
+            DATE=${DATETIME:14:10}
+            TIME=${DATETIME:25:8}
+            NEW_FILENAME=${DATE//:}_${TIME//:}.${EXT,,}
+        fi
+
+        mv "$FILE" $SOURCE/$ERROR_DIRECTORY/"$NEW_FILENAME"
+    done
 fi
 
 rm -f $PID_FILE
