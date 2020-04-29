e7b6a9eb889bd02b461a79617e5082e313cf8b1b
Gulivertx <me@gulivert.ch>
2020-04-29 15:14:11 +0200

    Code cleaning

--- synology-photos-auto-sort.sh
+++ synology-photos-auto-sort.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 ### INFO #################################################
-# Synology photos auto sort                              #
+# Synology photos and videos auto sort                              #
 # By Gulivert                                            #
 # https://github.com/Gulivertx/synology-photos-auto-sort #
 ##########################################################
 
-VERSION="0.3"
+VERSION="1.0"
 PID_FILE="/tmp/synology_photos_auto_sort.pid"
+LOG_DIRECTORY="logs"
+
+# Define a folder where the images will be moved for duplicate images and during a process error
 ERROR_DIRECTORY="error"
+
+### Allowed image and videos extensions
+ALLOWED_EXT="jpg JPG jpeg JPEG heic HEIC mov MOV heiv HEIV m4v M4V"
 
 echo "Synology photos and videos auto sort version $VERSION"
 echo "https://github.com/Gulivertx/synology-photos-auto-sort"
@@ -47,27 +53,27 @@ echo "Source folder : $SOURCE"
 echo "Target folder : $TARGET"
 echo ""
 
-### Allowed image extensions
-IMG_EXT=( "jpg" "JPG" "jpeg" "JPEG" "heic" "HEIC" )
-
-### Allowed video extensions
-VIDEO_EXT=( "mov" "MOV" "heiv" "HEIV" "m4v" "M4V")
-
-echo "Allowed image formats: ${IMG_EXT[@]}"
-echo "Allowed video formats: ${VIDEO_EXT[@]}"
+echo "Allowed formats: ${ALLOWED_EXT}"
 echo ""
 
 ### Move to source folder
 cd ${SOURCE}
 
-echo "Start image process"
+echo "Start process"
+
+# Count files
+FILES_COUNTER=$(ls *.* 2> /dev/null | wc -l | xargs)
+
+echo "$FILES_COUNTER files to process"
 echo ""
 
-for EXT in "${IMG_EXT[@]}"; do
-    FILES_COUNTER=$(ls *.${EXT} 2> /dev/null | wc -l)
+if [[ ${FILES_COUNTER} != 0 ]]; then
+    for FILE in *.*; do
+        FILENAME="${FILE%.*}" # Get filename
+        EXT="${FILE##*.}" # Get file extension
 
-    if [[ ${FILES_COUNTER} != 0 ]]; then
-        for FILE in *.${EXT}; do
+        # Verify if the extension is allowed
+        if [[ ${ALLOWED_EXT} == *"$EXT"* ]]; then
             DATETIME=$(exiftool ${FILE} | grep -i "create date" | head -1 | xargs)
 
             # Verify if we have exif data available
@@ -87,81 +93,51 @@ for EXT in "${IMG_EXT[@]}"; do
 
             # Move the file to target folder
             mv -n ${FILE} ${TARGET}/${YEAR}/${YEAR}.${MONTH}/${NEW_NAME}
-        done
+        fi
+    done
 
-        # Wait until the copy process is done
-        wait
+    # Wait until the process is done
+    wait
 
-        echo "All $EXT have been moved"
-        echo ""
-    fi
-done
+    echo "All have been moved"
+    echo ""
+fi
 
-echo "Start video process"
-echo ""
-
-for EXT in "${VIDEO_EXT[@]}"; do
-    FILES_COUNTER=$(ls *.${EXT} 2> /dev/null | wc -l)
-
-    if [[ ${FILES_COUNTER} != 0 ]]; then
-        for FILE in *.$EXT; do
-            DATETIME=$(exiftool ${FILE} | grep -i "create date" | head -1 | xargs)
-
-            # Verify if we have exif data available
-            if [[ -z ${DATETIME} ]]; then
-                continue
-            fi
-
-            # Extract date, time year and month and build new filename
-            DATE=${DATETIME:14:10}
-            TIME=${DATETIME:25:8}
-            NEW_NAME=${DATE//:}_${TIME//:}.${EXT,,}
-
-            # Create target folde
-            YEAR=${DATE:0:4}
-            MONTH=${DATE:5:2}
-            mkdir -p ${TARGET}/${YEAR}/${YEAR}.${MONTH}
-
-            # Move the file to target folder
-            mv -n ${FILE} ${TARGET}/${YEAR}/${YEAR}.${MONTH}/${NEW_NAME}
-        done
-
-        # Wait until the copy process is done
-        wait
-
-        echo "All $EXT have been moved"
-        echo ""
-    fi
-done
-
-### Move all files still not moved by the above rules in an error folder
+### Move all files still not moved by the above rule in an error folder
 UNMOVED_FILES_COUNTER=$(ls *.* 2> /dev/null | wc -l | xargs)
 
 if [[ ${UNMOVED_FILES_COUNTER} != 0 ]]; then
-    echo "There is $UNMOVED_FILES_COUNTER unmoved files, these files will be moved into error folder"
+    echo "$UNMOVED_FILES_COUNTER unmoved files, these files will be moved into error folder"
     echo ""
 
     mkdir -p ${SOURCE}/${ERROR_DIRECTORY}
 
+    # Create a new log file
+    CURRENT_DATE=`date +"%Y-%m-%d_%H-%M"`
+    LOG_FILE="${LOG_DIRECTORY}/${CURRENT_DATE}.log"
+    touch ${LOG_FILE}
+
     for FILE in *.*; do
-        # Get file extension
-        EXT="${FILE##*.}"
+        FILENAME="${FILE%.*}" # Get filename
+        EXT="${FILE##*.}" # Get file extension
 
-        DATETIME=$(exiftool ${FILE} | grep -i "create date" | head -1 | xargs)
+        # Verify if the extension is allowed
+        if [[ ${ALLOWED_EXT} == *"$EXT"* ]]; then
+            DATETIME=$(exiftool ${FILE} | grep -i "create date" | head -1 | xargs)
 
-        # Verify if we have exif data available
-        if [[ -z ${DATETIME} ]]; then
-            echo "No exif data available for image $FILE"
-            echo "Use original filename"
-            FILENAME="${FILE%.*}"
-            NEW_FILENAME=${FILENAME=//:}_exif_data_missing.${EXT,,}
-        else
-            DATE=${DATETIME:14:10}
-            TIME=${DATETIME:25:8}
-            NEW_FILENAME=${DATE//:}_${TIME//:}.${EXT,,}
+            # Verify if we have exif data available
+            if [[ -z ${DATETIME} ]]; then
+                NEW_FILENAME=${FILENAME=//:}_exif_data_missing.${EXT,,}
+                echo "No exif data available for image ${FILE}, moved into ${ERROR_DIRECTORY} and renamed as ${NEW_FILENAME}" >> ${LOG_FILE}
+            else
+                DATE=${DATETIME:14:10}
+                TIME=${DATETIME:25:8}
+                NEW_FILENAME=${DATE//:}_${TIME//:}.${EXT,,}
+                echo "An image with name ${NEW_FILENAME} already exist! File ${FILE} moved into ${ERROR_DIRECTORY} and renamed as ${NEW_FILENAME}" >> ${LOG_FILE}
+            fi
+
+            mv ${FILE} ${SOURCE}/${ERROR_DIRECTORY}/${NEW_FILENAME}
         fi
-
-        mv ${FILE} ${SOURCE}/${ERROR_DIRECTORY}/${NEW_FILENAME}
     done
 fi
 
