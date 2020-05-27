9748bae9ba7bc98f18f60c570bc35cb7bc011e5f
Gulivertx <me@gulivert.ch>
2020-05-27 21:02:44 +0200

    New version with base64 comparison when a file already exist with the same name (this comparison ensure that the file copied is not with the same content)

--- synology-photos-auto-sort.sh
+++ synology-photos-auto-sort.sh
@@ -1,25 +1,64 @@
 #!/bin/bash
 
 ### INFO #################################################
-# Synology photos and videos auto sort                              #
-# By Gulivert                                            #
-# https://github.com/Gulivertx/synology-photos-auto-sort #
+# Synology photos and videos auto sort
+# By Gulivert
+# https://github.com/Gulivertx/synology-photos-auto-sort
 ##########################################################
 
-VERSION="1.0"
+VERSION="1.1"
 PID_FILE="/tmp/synology_photos_auto_sort.pid"
 LOG_DIRECTORY="logs"
 
-# Define a folder where the images will be moved for duplicate images and during a process error
+# Define a folder where the images will be moved for process error
 ERROR_DIRECTORY="error"
+
+# Define a folder where the images will be moved for duplicate images
+DUPLICATE_DIRECTORY="duplicate"
 
 ### Allowed image and videos extensions
 ALLOWED_EXT="jpg JPG jpeg JPEG heic HEIC mov MOV heiv HEIV m4v M4V mp4 MP4"
 
-echo "Synology photos and videos auto sort version $VERSION"
-echo "https://github.com/Gulivertx/synology-photos-auto-sort"
-echo "______________________________________________________"
-echo ""
+FORCE_RENAME=false
+
+### HELP #################################################
+# Help
+##########################################################
+Help()
+{
+   # Display Help
+    echo "Synology photos and videos auto sort version $VERSION"
+    echo "https://github.com/Gulivertx/synology-photos-auto-sort"
+    echo "______________________________________________________"
+    echo "Needed dependencies"
+    echo "exiftool: https://exiftool.org/"
+    echo "______________________________________________________"
+    echo ""
+    echo "Arguments"
+    echo "source folder"
+    echo "target folder"
+    echo "Ex.: synology-photos-auto-sort.sh /path_to_source /path_to_target"
+    echo ""
+    echo "Options:"
+    #echo "-r     Force copy and rename duplicate images"
+    echo "-h     Print this Help."
+    echo ""
+}
+
+### OPTIONS ##############################################
+# Process the input options. Add options as needed
+##########################################################
+# Get the options
+while getopts ":h:r:" option; do
+   case ${option} in
+      h) # display Help
+         Help
+         exit
+         ;;
+      #r)
+      #   let FORCE_RENAME=true
+   esac
+done
 
 ### Verify if a script already running
 if [[ -f ${PID_FILE} ]]; then
@@ -43,9 +82,8 @@ SOURCE=$1
 TARGET=$2
 
 if [[ -z ${SOURCE} ]] || [[ -z ${TARGET} ]]; then
-    echo "Error: source and target folders are not specified as script arguments" >&2
-    echo "Ex.: synology-photos-auto-sort.sh /path_to_source /path_to_target"
     rm -f ${PID_FILE}
+    Help
     exit 1
 fi
 
@@ -71,6 +109,9 @@ FILES_ARR=(*.*)
 echo "$FILES_COUNTER files to process"
 echo ""
 
+# Move all allowed files to target folder
+MoveFiles()
+{
 if [[ ${FILES_COUNTER} != 0 ]]; then
     PROGRESS=0
     echo -ne "${PROGRESS}%\033[0K\r"
@@ -103,9 +144,9 @@ if [[ ${FILES_COUNTER} != 0 ]]; then
             # Move the file to target folder if not exist in target folder
             if [[ ! -f ${TARGET}/${YEAR}/${YEAR}.${MONTH}/${NEW_NAME} ]]; then
                 mv -n ${FILE} ${TARGET}/${YEAR}/${YEAR}.${MONTH}/${NEW_NAME}
-                let FILES_COUNTER--
 
                 # Remove the moved file from the array
+                let FILES_COUNTER--
                 FILES_ARR=("${FILES_ARR[@]/$FILE}")
             fi
         fi
@@ -120,16 +161,24 @@ if [[ ${FILES_COUNTER} != 0 ]]; then
     echo "All have been moved"
     echo ""
 fi
+}
+
+
 
 ### Move all files still not moved by the above rule in an error folder
+MoveUnmovedFiles()
+{
+TOTAL_FILES_COUNTER=${FILES_COUNTER}
+
 if [[ ${FILES_COUNTER} != 0 ]]; then
-    echo "$FILES_COUNTER unmoved files, these files will be moved into error folder"
+    echo "$FILES_COUNTER unmoved files, check for duplicate and process again..."
     echo ""
 
     PROGRESS=0
     echo -ne "${PROGRESS}%\033[0K\r"
 
     mkdir -p ${SOURCE}/${ERROR_DIRECTORY}
+    mkdir -p ${SOURCE}/${DUPLICATE_DIRECTORY}
     mkdir -p ${SOURCE}/${LOG_DIRECTORY}
 
     # Create a new log file
@@ -149,23 +198,72 @@ if [[ ${FILES_COUNTER} != 0 ]]; then
             if [[ -z ${DATETIME} ]]; then
                 NEW_FILENAME=${FILENAME=//:}_exif_data_missing.${EXT,,}
                 echo "No exif data available for image ${FILE}, moved into ${ERROR_DIRECTORY} and renamed as ${NEW_FILENAME}" >> ${LOG_FILE}
+                mv ${FILE} ${SOURCE}/${ERROR_DIRECTORY}/${NEW_FILENAME}
+
+                let PROGRESS++
+                echo -ne "$((${PROGRESS} * 100 / ${TOTAL_FILES_COUNTER}))%\033[0K\r"
+
+                # Remove the moved file from the array
+                let FILES_COUNTER--
+                FILES_ARR=("${FILES_ARR[@]/$FILE}")
+
+                continue
             else
                 DATE=${DATETIME:14:10}
                 TIME=${DATETIME:25:8}
                 NEW_FILENAME=${DATE//:}_${TIME//:}.${EXT,,}
-                echo "An image with name ${NEW_FILENAME} already exist! File ${FILE} moved into ${ERROR_DIRECTORY} and renamed as ${NEW_FILENAME}" >> ${LOG_FILE}
+
+                # Get target file path
+                YEAR=${DATE:0:4}
+                MONTH=${DATE:5:2}
+                TARGET_FILEPATH=${TARGET}/${YEAR}/${YEAR}.${MONTH}/${NEW_FILENAME}
+
+                # Test if existing file is the same
+                # Get base64 encoded image
+                SOURCE_FILE_BASE64="$(cat ${FILE} | base64)"
+                TARGET_FILE_BASE64="$(cat ${TARGET_FILEPATH} | base64)"
+
+                #See if files are the same
+                if [[ ${SOURCE_FILE_BASE64} == ${TARGET_FILE_BASE64} ]]; then
+                    NEW_FILENAME=${FILENAME=//:}_duplicate.${EXT,,}
+                    echo "The file ${FILE} already exist and is the same! File ${FILE} moved into ${DUPLICATE_DIRECTORY} and renamed as ${NEW_FILENAME}" >> ${LOG_FILE}
+                    mv ${FILE} ${SOURCE}/${DUPLICATE_DIRECTORY}/${NEW_FILENAME}
+
+                    let PROGRESS++
+                    echo -ne "$((${PROGRESS} * 100 / ${TOTAL_FILES_COUNTER}))%\033[0K\r"
+
+                    # Remove the moved file from the array
+                    let FILES_COUNTER--
+                    FILES_ARR=("${FILES_ARR[@]/$FILE}")
+
+                    continue
+                else
+                    # Generate a unique ID
+                    #UUID=$(cat /proc/sys/kernel/random/uuid)
+                    UUID="U$(date +%s%N)"
+
+                    NEW_FILENAME=${FILENAME=//:}_${UUID}.${EXT,,}
+
+                    mv -n ${FILE} ${TARGET}/${YEAR}/${YEAR}.${MONTH}/${NEW_NAME}
+
+                    let PROGRESS++
+                    echo -ne "$((${PROGRESS} * 100 / ${TOTAL_FILES_COUNTER}))%\033[0K\r"
+
+                    # Remove the moved file from the array
+                    let FILES_COUNTER--
+                    FILES_ARR=("${FILES_ARR[@]/$FILE}")
+                fi
             fi
-
-            mv ${FILE} ${SOURCE}/${ERROR_DIRECTORY}/${NEW_FILENAME}
         fi
-
-        let PROGRESS++
-        echo -ne "$((${PROGRESS} * 100 / ${FILES_COUNTER}))%\033[0K\r"
     done
 
     # Wait until the process is done
     wait
 fi
+}
+
+MoveFiles
+MoveUnmovedFiles
 
 # Clean @eaDir
 if [[ -d "${SOURCE/}@eaDir" ]]; then
